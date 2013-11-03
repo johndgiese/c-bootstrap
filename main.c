4ceb4e7778e2f9eac0826358864b781b88218392
johndgiese <johndgiese@gmail.com>
2013-11-03 12:14:51 -0500

    Add separate header file.

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
 #include <iso646.h>
 #include <stdio.h>
 
+#include "util.h"
+
 int main(int argc, const char *argv[]) {
-    printf("Hello World!\n");
+    float a, b;
+
+    printf("Enter a number: ");
+    scanf("%f", &a);
+    printf("Enter a number: ");
+    scanf("%f", &b);
+
+    printf("Sum: %f\n", add(a, b));
+    printf("Difference: %f\n", subtract(a, b));
+
     return 0;
 }
